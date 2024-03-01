487f50d1bc890acb93c10ace69eab2ff4b3967c9
lukeb <lukejbrooks@gmail.com>
2024-03-01 12:50:58 -0800

    first commit

--- crash_course/settings.py
+++ crash_course/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [
+    Path(BASE_DIR,'static')
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
