db625ec5580459a483fcf4a83403031663f97d6e
lukeb <lukejbrooks@gmail.com>
2024-03-04 11:50:40 -0800

    second commit

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,18 @@ from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+
+
+    context = {'orders': orders, 'customers': customers, 'total_orders': total_customers, 'delivered': delivered, 'pending':pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
     products = Product.objects.all()
