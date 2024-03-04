5b4da227aba764ce5a3c163efb83d358bddcb201
lukeb <lukejbrooks@gmail.com>
2024-03-04 12:36:29 -0800

    update customer pages

--- accounts/views.py
+++ accounts/views.py
@@ -13,15 +13,16 @@ def home(request):
     total_orders = orders.count()
     delivered = orders.filter(status = 'Delivered').count()
     pending = orders.filter(status = 'Pending').count()
-
-
     context = {'orders': orders, 'customers': customers, 'total_orders': total_customers, 'delivered': delivered, 'pending':pending}
-
     return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products':products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request,pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders = customer.order_set.all()
+    order_count= orders.count()
+    context = {'customer':customer, 'orders': orders,'order_count':order_count}
+    return render(request, 'accounts/customer.html',context)
