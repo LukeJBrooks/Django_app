c52d4eb27345c68bb4711c2732eea70b22bb0518
lukeb <lukejbrooks@gmail.com>
2024-03-06 12:10:58 -0800

    big update, CRUD functionality

--- accounts/models.py
+++ accounts/models.py
@@ -38,13 +38,17 @@ class Order(models.Model):
     STATUS = (
         ('Pending','Pending'),
         ('Out for delivery','Out for delivery'),
-        ('Delivered','Deliverd'),
+        ('Delivered','Delivered'),
         )
 
     customer = models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
     product = models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True,choices=STATUS)
+
+    def __str__(self):
+        return self.product.name
+
 
     
 
