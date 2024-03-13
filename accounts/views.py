db713abd0d74f6ecb3f2d2829d0a019be201f6d6
lukeb <lukejbrooks@gmail.com>
2024-03-13 11:03:12 -0700

    login and logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib import messages 
+
+from django.contrib.auth import authenticate, login, logout
+
+
 from .models import * 
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
@@ -14,6 +19,8 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user=form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
             return redirect('login')
 
 
@@ -22,8 +29,27 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect')
     context={}
     return render(request, 'accounts/login.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
+
 
 def home(request):
     orders = Order.objects.all()
